e8e1d2878da8172774bd4b0b966f480a6cbcc5ef
Ismail Yilmaz <ismyilson@gmail.com>
2023-02-24 21:01:00 +0100

    Add text to speech reader

---
+++ reader.py
@@ -0,0 +1,13 @@
+import pyttsx3
+
+
+engine = pyttsx3.init()
+engine.setProperty('rate', 130)
+
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+def read_text(text):
+    engine.say(text)
+    engine.runAndWait()
