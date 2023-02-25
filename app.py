f66411fb733c138e6e7a1c143a44d87b031e6cbf
Ismail Yilmaz <ismyilson@gmail.com>
2023-02-25 01:45:10 +0100

    Refactor audio_handler

--- app.py
+++ app.py
@@ -1,22 +1,20 @@
-from handlers import audio_handler as ah, file_handler
+from handlers import audio_handler, file_handler
 
 import processor as pr
 import transcriber
 
 
 class App:
-    audio_handler: ah.AudioHandler
     processor: pr.Processor
 
     def __init__(self):
-        self.audio_handler = ah.AudioHandler()
         self.processor = pr.Processor()
 
-        self.sample_size = self.audio_handler.get_sample_size()
+        self.sample_size = audio_handler.get_sample_size()
 
     def start(self):
         while True:
-            frames = self.audio_handler.record()
+            frames = audio_handler.record()
 
             with file_handler.audio_to_file(frames, self.sample_size) as audio_file:
                 user_input = transcriber.transcribe(audio_file)
--- handlers/audio_handler.py
+++ handlers/audio_handler.py
@@ -9,45 +9,38 @@ CHUNK = 1024
 RECORD_SECONDS = 30
 
 
-class AudioHandler:
-    def __init__(self):
-        self.audio = pyaudio.PyAudio()
-        self.stream = self.audio.open(format=FORMAT,
-                                      channels=CHANNELS,
-                                      rate=RATE,
-                                      input=True,
-                                      frames_per_buffer=CHUNK,
-                                      start=False)
+audio = pyaudio.PyAudio()
+stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK, start=False)
 
-        self.sample_size = self.audio.get_sample_size(FORMAT)
 
-    def record(self):
-        just_started = True
-        retries = 0
-        frames = []
+def get_sample_size():
+    return audio.get_sample_size(FORMAT)
 
-        self.stream.start_stream()
 
-        print('Recording...')
-        for i in range(int(RATE / CHUNK * RECORD_SECONDS)):
-            data = np.frombuffer(self.stream.read(CHUNK), dtype=np.int16)
-            peak = np.average(np.abs(data)) * 2
+def record():
+    just_started = True
+    retries = 0
+    frames = []
 
-            if int(50 * peak / 2 ** 16) < 1:
-                retries += 1
-            elif just_started:
-                retries = 0
-                just_started = False
+    stream.start_stream()
 
-            if retries >= 75 and not just_started:
-                break
+    print('Recording...')
+    for i in range(int(RATE / CHUNK * RECORD_SECONDS)):
+        data = np.frombuffer(stream.read(CHUNK), dtype=np.int16)
+        peak = np.average(np.abs(data)) * 2
 
-            frames.append(data)
-        print('Recording stop')
+        if int(50 * peak / 2 ** 16) < 1:
+            retries += 1
+        elif just_started:
+            retries = 0
+            just_started = False
 
-        self.stream.stop_stream()
+        if retries >= 75 and not just_started:
+            break
 
-        return frames
+        frames.append(data)
+    print('Recording stop')
 
-    def get_sample_size(self):
-        return self.audio.get_sample_size(FORMAT)
+    stream.stop_stream()
+
+    return frames
